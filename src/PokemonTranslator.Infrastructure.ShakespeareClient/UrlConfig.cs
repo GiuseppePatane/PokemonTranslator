507f191793cf5fc11eefca3a950efa6500cbb83a
Giuseppe Patanè <giuseppe.patane@vivaticket.com>
2021-01-14 10:07:38 +0100

    Initial Shakespeare api logic  using httpclient

---
+++ src/PokemonTranslator.Infrastructure.ShakespeareClient/UrlConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokemonTranslator.Infrastructure.ShakespeareClient
+{
+    public static class UrlConfig
+    {
+        public static string GetShakespeareTranslation(string text)
+        {
+            var queryString = BuildQueryString(new Dictionary<string, string>
+            {
+                {"text", text},
+            });
+            var url = $"translate/shakespeare.json?{queryString}";
+            return url;
+        }
+        
+        
+        private static string BuildQueryString(Dictionary<string, string> keyValueQueryString)
+        {
+            var validParameters =
+                keyValueQueryString.Where(x => !string.IsNullOrEmpty(x.Key) && !string.IsNullOrEmpty(x.Value));
+            return string.Join("&",
+                validParameters.Select(x => $"{HttpUtility.UrlEncode(x.Key)}={HttpUtility.UrlEncode(x.Value)}"));
+        }
+    }
+}
