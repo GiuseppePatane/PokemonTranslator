1edbd08fc7440da723df58652595e97ad87656a8
Giuseppe Patanè <giuseppe.patane@vivaticket.com>
2021-01-15 16:11:08 +0100

    remove wrong reference thant make docker run fail

---
+++ src/PokemonTranslator.Infrastructure/RegisterClients.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PokeApiNet;
+using PokemonTranslator.Core.Interfaces;
+using PokemonTranslator.Infrastructure.PokemonCient;
+using PokemonTranslator.Infrastructure.ShakespeareClient;
+
+namespace PokemonTranslator.Infrastructure
+{
+    public static class RegisterClients
+    {
+        public static IServiceCollection AddClients(this IServiceCollection services)
+        {
+            services.AddTransient<PokeApiClient>();
+            services.AddTransient<IPokemonClient, PokeClient>();
+            return services;
+        }
+
+        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient<ITranslatorClient, ShakespeareHttpClient>(client =>
+            {
+                client.BaseAddress = new Uri(configuration["HttpClients:ShakespeareApiBaseUrl"]);
+            });
+            return services;
+        }
+    }
+}
