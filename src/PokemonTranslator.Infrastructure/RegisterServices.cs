fe664979db8a0520acf986026feee5f6f0e8cd5e
Giuseppe Patanè <gpatane13@gmail.com>
2021-01-15 17:23:35 +0100

    Feature/docker (#2)
    * remove wrong reference thant make docker run fail
    
    * remove wrong reference thant make docker run fail
    
    * add docker-compose
    
    Co-authored-by: Giuseppe Patanè <giuseppe.patane@vivaticket.com>

--- src/PokemonTranslator.Infrastructure/RegisterServices.cs
+++ src/PokemonTranslator.Infrastructure/RegisterServices.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using PokeApiNet;
+﻿using Microsoft.Extensions.DependencyInjection;
 using PokemonTranslator.Core.Interfaces;
 using PokemonTranslator.Core.Services;
-using PokemonTranslator.Infrastructure.PokemonCient;
-using PokemonTranslator.Infrastructure.ShakespeareClient;
 
 namespace PokemonTranslator.Infrastructure
 {
@@ -14,25 +9,6 @@ namespace PokemonTranslator.Infrastructure
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
             services.AddTransient<ITranslatorService, TranslatorService>();
-            return services;
-        }
-    }
-
-    public static class RegisterClients
-    {
-        public static IServiceCollection AddClients(this IServiceCollection services)
-        {
-            services.AddTransient<PokeApiClient>();
-            services.AddTransient<IPokemonClient, PokeClient>();
-            return services;
-        }
-
-        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddHttpClient<ITranslatorClient, ShakespeareHttpClient>(client =>
-            {
-                client.BaseAddress = new Uri(configuration["HttpClients:ShakespeareApiBaseUrl"]);
-            });
             return services;
         }
     }
